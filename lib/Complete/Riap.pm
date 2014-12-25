a0fd21b6dae065fd18ecdc6ac9240164818eb9d7
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-25 14:38:46 +0700

    Use Complete::Path, to avoid code duplication and gain feature like case-insensitive matching, map case, and expand immediate paths

--- lib/Complete/Riap.pm
+++ lib/Complete/Riap.pm
@@ -7,7 +7,7 @@ use 5.010001;
 use strict;
 use warnings;
 
-#use Complete;
+use Complete;
 
 our %SPEC;
 require Exporter;
@@ -34,6 +34,16 @@ _
             req => 1,
             pos => 0,
         },
+        ci => {
+            summary => 'Whether to do case-insensitive search',
+            schema  => 'bool*',
+        },
+        map_case => {
+            schema => 'bool',
+        },
+        exp_im_path => {
+            schema => 'bool',
+        },
         type => {
             schema => ['str*', in=>['function','package']], # XXX other types?
             summary => 'Filter by entity type',
@@ -42,43 +52,57 @@ _
     result_naked => 1,
 };
 sub complete_riap_url {
+    require Complete::Path;
+
     my %args = @_;
 
     my $word = $args{word} // ''; $word = '/' if !length($word);
+    $word = "/$word" unless $word =~ m!\A/!;
+    my $ci          = $args{ci} // $Complete::OPT_CI;
+    my $map_case    = $args{map_case} // $Complete::OPT_MAP_CASE;
+    my $exp_im_path = $args{exp_im_path} // $Complete::OPT_EXP_IM_PATH;
     my $type = $args{type} // '';
 
-    my $scheme;
-    if ($word =~ m!\A/!) {
-        $scheme = '';
+    my $starting_path;
+    my $result_prefix = '';
+    if ($word =~ s!\A/!!) {
+        $starting_path = '/';
+        $result_prefix = '/';
     } elsif ($word =~ s!\Apl:/!/!) {
-        $scheme = 'pl';
+        $starting_path = 'pl:';
+        $result_prefix = 'pl:';
     } else {
         return [];
     }
 
-    my ($pkg, $leaf) = $word =~ m!(.*/)(.*)!;
-    state $pa = do {
-        require Perinci::Access;
-        Perinci::Access->new;
-    };
+    my $res = Complete::Path::complete_path(
+        word => $word,
+        ci => $ci, map_case => $map_case, exp_im_path => $exp_im_path,
+        list_func => sub {
+            my ($path, $intdir, $isint) = @_;
 
-    my $riap_res = $pa->request(list => $pkg, {detail=>1});
-    return [] unless $riap_res->[0] == 200;
+            state $pa = do {
+                require Perinci::Access;
+                Perinci::Access->new;
+            };
 
-    my @res;
-    for my $ent (@{ $riap_res->[2] }) {
-        next unless $ent->{type} eq 'package' ||
-            (!$type || $type eq $ent->{type});
-        next unless index($ent->{uri}, $leaf) == 0;
-        push @res, "$pkg$ent->{uri}";
-    }
+            $path = "/$path" unless $path =~ m!\A/!;
+            my $riap_res = $pa->request(list => $path, {detail=>1});
+            return [] unless $riap_res->[0] == 200;
+            my @res;
+            for my $ent (@{ $riap_res->[2] }) {
+                next unless $ent->{type} eq 'package' ||
+                    (!$type || $type eq $ent->{type});
+                push @res, $ent->{uri};
+            }
+            \@res;
+        },
+        starting_path => $starting_path,
+        result_prefix => $result_prefix,
+        is_dir_func => sub { }, # not needed, we already suffixed "dir" with /
+    );
 
-    # put scheme back on
-    if ($scheme) {
-        for (@res) { $_ = "$scheme:$_" }
-    }
-
-    {words=>\@res, path_sep=>'/'};
+    {words=>$res, path_sep=>'/'};
 }
 
 1;
